36898363f081aee87d381a4b98a593ad3189bb2e
张恒鑫 <zhanghengxin@buaa.edu.cn>
2024-12-17 16:23:39 +0800

    更新数据库的密码，保存到本地

---
+++ DataBase/DataBaseConfig.py
@@ -0,0 +1,23 @@
+configs = [
+        {
+            'host': '112.4.115.127',
+            'port': 3312,
+            'database': 'zhx_database1',
+            'user': 'zhx_0001',
+            'password': 'CgVrZcbLp87Juj5j'
+        },
+        {
+            'host': '112.4.115.127',
+            'port': 3312,
+            'database': 'zhx_database2',
+            'user': 'zhx_0001',
+            'password': 'CgVrZcbLp87Juj5j'
+        },
+        {
+            'host': '112.4.115.127',
+            'port': 3312,
+            'database': 'zhx_database3',
+            'user': 'zhx_0001',
+            'password': 'CgVrZcbLp87Juj5j'
+        }
+    ]
---
+++ FederatedDatabase/FederationConfig.py
@@ -0,0 +1,7 @@
+federated_config = {
+    'host': '112.4.115.127',
+    'port': 3312,
+    'database': 'zhx_0001',
+    'user': 'zhx_0001',
+    'password': 'CgVrZcbLp87Juj5j'
+}
